5955538e04eb758b4d3a7d2ca148522924f45915
keitsengSIR <miguelkeitseng5@gmail.com>
2026-06-01 11:50:16 +0200

    Initial commit of portfolio

---
+++ script.js
@@ -0,0 +1,16 @@
+const fadeElements = document.querySelectorAll('.fade-in');
+
+window.addEventListener('scroll', () => {
+
+    fadeElements.forEach(element => {
+
+        const position = element.getBoundingClientRect().top;
+        const screenPosition = window.innerHeight / 1.3;
+
+        if(position < screenPosition) {
+            element.classList.add('show');
+        }
+
+    });
+
+});
