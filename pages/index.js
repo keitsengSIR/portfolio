bef3c0d6397883f66885c626300c256ff8bc49c8
keitsengSIR <miguelkeitseng5@gmail.com>
2026-06-09 15:27:41 +0200

    Fix CSS path and homepage content

--- pages/index.js
+++ pages/index.js
@@ -51,7 +51,8 @@ export default function Home() {
         <title>Miguel Kesego Keitseng | Portfolio</title>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="stylesheet" href="/style.css" />
+        <link rel="stylesheet" href="/assets/style.css" />
+
       </Head>
 
       {/* Navigation */}
